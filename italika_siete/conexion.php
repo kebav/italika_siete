ddf67f546bb08678a7d32506a2b66f4c3194295d
kebav <correoparalaescuela79@gmail.com>
2026-05-03 15:13:10 -0600

    conexion.php

--- italika_siete/conexion.php
+++ italika_siete/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("%","dev_user","User*2026","tso2026");
+$conexion = mysqli_connect("localhost","dev_user","User*2026","tso2026");
 
 if(!$conexion){
     die("Error de conexión");
