fd08be5f71e5cd5c771baafb65d08f38aed9ae91
kebav <correoparalaescuela79@gmail.com>
2026-05-25 19:53:55 -0600

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,8 @@
-<?php
-$conexion = mysqli_connect("localhost","dev_user","User*2026","tso2026");
+<?php 
+$conexion = 
+mysqli_connect("localhost","dev_user_axel","User*2026","tso2026"); 
 
-if(!$conexion){
-    die("Error de conexión");
-}
+if(!$conexion){ 
+    die("Error de conexión"); 
+} 
 ?>
